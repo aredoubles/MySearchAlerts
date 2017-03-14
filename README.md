bed504bf5c8e964cbc0be1c5cea20a20059d889d
aredoubles <roger.w.shaw@gmail.com>
2017-03-14 15:35:04 -0400

    Basic search

---
+++ README.md
@@ -0,0 +1,9 @@
+# Twitter Search
+
+I want to use the Twitter API to do searches for certain terms.
+
+Currently, this script asks for a term, then prints the results in the command line.
+
+Down the line, I want to get alerts, even emails, on a list of custom terms.
+
+Mostly just an excuse to see the Twitter API!
---
+++ TwitterSearch.py
@@ -0,0 +1,48 @@
+from twitter import *
+import json
+import os
+
+# OAuth tokens grabbed from my environment, amazingly!
+auth = OAuth(
+    consumer_key = os.environ['twitter_consumer_key'],
+    consumer_secret = os.environ['twitter_consumer_secret'],
+    token = os.environ['twitter_token'],
+    token_secret = os.environ['twitter_token_secret']
+)
+
+t = Twitter(auth=auth)
+
+term = input('What term would you like to search for?\nUse quotation marks if desired\n')
+
+# Do a search!
+res = t.search.tweets(q='''%s''' % term)
+
+# Save as a json file
+'''with open('res.json', 'w') as outfile: json.dump(res, outfile)'''
+
+# Parse out the text (for the first status)
+'''res['statuses'][0]['text']'''
+# How can I find out how many results there are?
+# Apparently can't slice like [:]
+
+# If no results, print that
+if res['statuses'] == []:
+    print('No results found!')
+
+
+# Iterate through each tweet/status
+for tweet in res['statuses']:
+    try:
+        # Read in one line of the file, convert it into a json object
+        # print(tweet['id'] # This is the tweet's id
+        print(tweet['created_at']) # when the tweet posted
+        print(tweet['text']) # content of the tweet
+
+        # print(tweet['user']['id'] # id of the user who posted the tweet
+        print(tweet['user']['name']) # name of the user, e.g. "Wei Xu"
+        # print(tweet['user']['screen_name'] # name of the user account, e.g. "cocoweixu"
+
+    except:
+        # read in a line is not in JSON format (sometimes error occured)
+        print('Iterating on the results didnt seem to work')
+        continue
